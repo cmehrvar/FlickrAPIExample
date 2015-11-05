38536dc1078737aa6f66085a5f86ad9a45d9765c
cmehrvar <cinamehrvar@gmail.com>
2015-11-05 16:15:26 -0500

    thanks james!

--- askJamesForHelp/askJamesForHelp/ViewController.swift
+++ askJamesForHelp/askJamesForHelp/ViewController.swift
@@ -8,8 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UIGestureRecognizerDelegate {
    
     @IBOutlet var myTableView: UITableView!
     
@@ -20,7 +19,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 9
     }
     
     
@@ -33,7 +32,50 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func longPressed(sender: UILongPressGestureRecognizer) {
+        print("Long Pressed")
+        guard let cell = sender.view as? myTableViewCell else {return}
+    
+        switch sender.state {
+            case .Began:
+                cell.toggleCellImage()
+                myTableView.userInteractionEnabled = false
+            case .Ended:
+                cell.toggleCellImage()
+             myTableView.userInteractionEnabled = true
+            
+            default:
+                print("default state")
+        }
+    }
+    
+    
+    @IBAction func panOnCell(sender: UIPanGestureRecognizer) {
+        guard let cell = sender.view as? myTableViewCell, image = cell.myCellImage else {return}
+        let translation = sender.translationInView(view)
+        
+        switch sender.state {
+        case .Changed:
+            if cell.isExpanded {
+                cell.cellCentringConstraint.constant = translation.x
+            }
+        case .Ended:
+           // if cell.isExpanded {
+                UIView.animateWithDuration(0.3, animations: { () -> Void in
+                    cell.cellCentringConstraint.constant = 0
+                })
+                
+           // }
+        default:
+            print("default state")
+        }
+    }
+    
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
 
-
+    
 }
 
--- askJamesForHelp/myTableViewCell.swift
+++ askJamesForHelp/myTableViewCell.swift
@@ -10,13 +10,17 @@ import UIKit
 
 class myTableViewCell: UITableViewCell {
     
+    var isExpanded = false
     
     @IBOutlet weak var myCellImage: UIImageView!
     
+    @IBOutlet weak var cellCentringConstraint: NSLayoutConstraint!
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        andGestures()
+        
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
@@ -25,4 +29,76 @@ class myTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+    func andGestures() {
+        let longPressRecogniser = UILongPressGestureRecognizer(target: self, action: "longPressed:")
+        longPressRecogniser.delegate = self
+        self.addGestureRecognizer(longPressRecogniser)
+        let panRecogniser = UIPanGestureRecognizer(target: self, action: "panOnCell:")
+        panRecogniser.delegate = self
+        self.addGestureRecognizer(panRecogniser)
+    }
+    
+    func toggleCellImage() {
+        UIView.animateWithDuration(0.3) {
+            () -> Void in
+            if !self.isExpanded {
+                self.myCellImage?.transform =  CGAffineTransformMakeScale(1.1, 1.1)
+            } else {
+                self.myCellImage?.transform =  CGAffineTransformIdentity
+            }
+        }
+        isExpanded = !isExpanded
+    }
+    
+    func expandImage() {
+        UIView.animateWithDuration(0.3) {
+            () -> Void in
+            self.myCellImage?.transform =  CGAffineTransformMakeScale(1.1, 1.1)
+        }
+        isExpanded = true
+    }
+    func contractImage() {
+        UIView.animateWithDuration(0.3) {
+            () -> Void in
+            self.myCellImage?.transform = CGAffineTransformIdentity
+        }
+        isExpanded = false
+    }
+    
+    
+    
+    func longPressed(sender: UILongPressGestureRecognizer) {
+        print("Long Pressed")
+        switch sender.state {
+        case .Began:
+            expandImage()
+        case .Ended:
+            contractImage()
+        default:
+            print("default state")
+        }
+    }
+    
+    func panOnCell(sender: UIPanGestureRecognizer) {
+        let translation = sender.translationInView(self)
+        switch sender.state {
+        case .Changed:
+            if isExpanded {
+                cellCentringConstraint.constant = translation.x
+            }
+        case .Ended:
+            UIView.animateWithDuration(0.3, animations: { () -> Void in
+                self.cellCentringConstraint.constant = 0
+            })
+        default:
+            print("default state")
+        }
+    }
+    
+    override func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+
+    
+    
 }
